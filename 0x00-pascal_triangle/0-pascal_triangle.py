eb3c3b6242d1ec91ee71db0920fe35a2529c2a33
Yara-ib <yr.ap.23@gmail.com>
2024-07-08 01:10:39 +0300

    Pascal Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" Pascal's Triangle Module """
+from typing import List
+
+
+def pascal_triangle(n: int) -> List[List]:
+    """
+    Function to get list of Pascal's triangle rows & their values within
+
+    Args:
+        n (int): number of rows representing the Pascal's triangle height
+
+    Returns:
+        List[List]: integers representing the Pascal's triangle of height(n)
+    """
+
+    # Checking the value of the given height for the triangle
+    if n <= 0:
+        return []
+
+    # Forming an empty list to add numbers to each row forming the triangle,
+    # according to the height & how pascal triangle items should be calculated
+    pascal_pyramid_list = []
+
+    for _ in range(n):
+        # Adding the first number at each row = "1"
+        row = [1]
+
+        # If n: height of the triangle > 0
+        if pascal_pyramid_list:
+            # Naming that the last item in each row to use it as reference
+            last_item_in_row = pascal_pyramid_list[-1]
+
+            # idx: 0 --> -1 in the row's length
+            for idx in range(len(last_item_in_row) - 1):
+                row.append(last_item_in_row[idx] + last_item_in_row[idx + 1])
+
+            # Adding the last number at each row = "1"
+            row.append(1)
+
+        pascal_pyramid_list.append(row)
+
+    return pascal_pyramid_list
