aa21939e14bfb79f67c0908428eea951929a5b12
Yara-ib <yr.ap.23@gmail.com>
2024-09-20 03:03:28 +0300

    Change comes from within Task

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Change comes from within Module """
+
+
+def makeChange(coins: list[int], total: int) -> int:
+    """
+    Get the fewest number of coins needed to meet a given amount total
+
+    Args:
+        coins (list[int]): List of the values of the coins in your possession
+        total (int): Given amount
+
+    Returns:
+        int: fewest number of coins needed to meet total
+    """
+
+    if total <= 0:
+        return 0
+
+    countNeeded = 0
+    trackCoinsCount = 0
+    # To check the largest value coin getting the least coins needed
+    coins.sort(reverse=True)
+
+    for coin in coins:
+        trackCoinsCount = total // coin
+        countNeeded += trackCoinsCount
+        total -= trackCoinsCount * coin
+
+        if total == 0:
+            return countNeeded
+
+    if total > 0:
+        return -1
