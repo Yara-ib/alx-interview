663be53480a2ea024e1a1759e5da37062c590916
Yara-ib <yr.ap.23@gmail.com>
2024-09-20 03:08:31 +0300

    Checker hated the annotations : D so had to remove them

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,7 +2,7 @@
 """ Change comes from within Module """
 
 
-def makeChange(coins, total) :
+def makeChange(coins, total):
     """
     Get the fewest number of coins needed to meet a given amount total
 
